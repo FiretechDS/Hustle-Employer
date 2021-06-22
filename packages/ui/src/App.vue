97f21f4c44c8ab5020ef07687a01699d9981f7be
siletnbliss <luiscasm2501@gmail.com>
2021-06-22 11:53:47 -0400

    UI: base de barra de navegación y test de pull request

--- packages/ui/src/App.vue
+++ packages/ui/src/App.vue
@@ -1,27 +1,41 @@
 <template>
- <div class="container">
-   Hello
- </div>
+  <div class="main">
+    <NavBar/> 
+    <div class="container">
+
+      Hello
+
+    </div>
+  </div>
 
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-
+import NavBar from './components/nav-bar/NavBar.vue'
 
 export default defineComponent({
   name: "App",
   components: {
-    
+    NavBar
   },
 });
 </script>
 
-<style lang="scss">
+<style lang="scss" >
+html, body {
+  margin: 0px !important;
+  padding: 0px !important;
+  font-family: 'Poppins', sans-serif;
+}
+.main{
+  width: 100%;
+}
 .container{
  background-color: $background-color;
- color: $white;
- min-height: 100vh;
+ margin:0;
+ min-height: 80vh;
  font-size: $normal-font;
+ color: $white;
 }
 </style>
