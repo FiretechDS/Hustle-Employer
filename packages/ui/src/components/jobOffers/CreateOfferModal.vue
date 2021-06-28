cecf089d1ef961ac283bd7e49e81dafda08521c2
sebasgonzz <sebbaas99@gmail.com>
2021-06-28 00:18:54 -0400

    ADD - Crear Oferta de Trabajo: Componente UI principal del CU. Componente principal de la interfaz en el que se muestra el formulario correspondiente para la creación de ofertas de trabajos. El componente CreateOfferModal.vue aplica SRP y "grita" el CU. Commit de los avances alcanzados este fin de semana. Adicionalmente, se añade un nuevo asset para un botón de confirmación.

--- packages/ui/src/components/jobOffers/CreateOfferModal.vue
+++ packages/ui/src/components/jobOffers/CreateOfferModal.vue
@@ -1,23 +1,55 @@
 <template>
   <div>
     <PlusButton :onClick="showModal" />
-    <Modal v-show="state.isModalVisible" @close="closeModal()">
-      <template v-slot:header> Crear oferta </template>
+    <Modal v-show="state.isModalVisible" @close="closeModal">
+      <template v-slot:header> 
+        <h2>Crear nueva oferta</h2> 
+      </template>
+      <template v-slot:body>
+        <div class="create-job-offer-body-div">
+          <form class="create-job-offer-form">
+            <input v-model="jobOffer.title" placeholder="Título">
+            <input class="description-input" v-model="jobOffer.description" placeholder="Descripción">
+            <input v-model="jobOffer.deadline" placeholder="Fecha tope (DD/MM/YYYY)">
+            <input v-model="jobOffer.duration" placeholder="Duración (horas)">
+            <input v-model="jobOffer.hourlyRate" placeholder="Salario/Hora">
+            <input v-model="jobOffer.schedules[0]" placeholder="Horarios">
+            <input v-model="jobOffer.skills[0]" placeholder="Habilidades">
+          </form>
+        </div>
+      </template>
+      <template v-slot:footer>
+        <div class="create-job-offer-footer">
+          <Button buttonText="Crear Oferta" iconName="paper-plane.svg" isPrimary=true :onClick="createOffer"/> 
+        </div>
+      </template>
     </Modal>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from "vue";
-import PlusButton from "@/components/PlusButton.vue";
+import { defineComponent, PropType, reactive } from "vue";
+import PlusButton from "../PlusButton.vue";
 import Modal from "../Modal.vue";
+import Button from "../Button.vue"
 export default defineComponent({
   name: "CreateOfferModal",
-  components: { Modal, PlusButton },
+  components: { Modal, PlusButton, Button },
   setup() {
     const state = reactive({
-      isModalVisible: false as boolean,
+      isModalVisible: false as boolean
     });
+
+    const jobOffer = reactive({
+      title: "" as string,
+      description: "" as string,
+      deadline: "" as string,
+      duration: "" as string,
+      hourlyRate: "" as string,
+      schedules: [] as Array<string>,
+      skills: [] as Array<string>,
+    });
+
     function showModal(): void {
       state.isModalVisible = true;
       console.log("open");
@@ -26,11 +58,48 @@ export default defineComponent({
       state.isModalVisible = false;
       console.log("closed");
     }
+
+    function createOffer(): void {
+      console.log("created");
+    }
+
+
     return {
       state,
+      jobOffer,
       showModal,
       closeModal,
+      createOffer
     };
   },
 });
 </script>
+
+<style lang="scss" scoped>
+
+div{
+  color: grey;
+}
+
+input{
+  font-family: 'Poppins';
+  margin: 10px;
+  display: inline-block;
+  border-radius: 10px;
+  border-color: rgb(196, 196, 196);
+  padding: 5px;
+  width: 20%;
+  background: rgba(197, 197, 197, 0.349);
+}
+
+Button{
+  margin-left: auto;
+  
+}
+
+.description-input{
+  width: 80%;
+
+}
+
+</style>
